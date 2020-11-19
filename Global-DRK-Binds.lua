5e38bedc667cb4bad7e5d8ff205f19eb882ae1b3
ArislanShiva <jonathan.p.tao@gmail.com>
2020-11-19 11:03:44 -0600

    Updated Binds

---
+++ Global-DRK-Binds.lua
@@ -0,0 +1,27 @@
+    send_command('bind #` input //send @others /attack <t>')
+
+    --send_command('bind #1 input //send @others /ma "Stun" <t>')
+    send_command('bind #2 input //send @others /ja "Dark Seal" <me>')
+    send_command('bind #3 input //send @others /ja "Nether Void" <me>')
+    send_command('bind #4 input //send @others /ja "Weapon Bash" <t>')
+    --send_command('bind #5 input //send @others /ja "Dark Seal" <me>')
+    send_command('bind #6 input //send @others /ja "Nether Void" <me>')
+    send_command('bind #7 input //send @others /ma "Sleep II" <t>')
+    send_command('bind #8 input //send @others /ma "Sleepga" <t>')
+    send_command('bind #9 input //send @others /ma "Absorb-TP" <t>')
+    send_command('bind #0 input //send @others /ma "Stun" <t>')
+
+    send_command('bind #q input //send @others /ma "Drain III" <t>')
+    send_command('bind #w input //send @others /ja "Last Resort" <me>')
+    send_command('bind #e input //send @others /ma "Hasso" <me>')
+    send_command('bind #p input //send @others /ma "Dread Spikes" <me>')
+
+    send_command('bind #numlock input //send @others /ja "Souleater" <me>')
+    send_command('bind #numpad/ input //send @others /ja "Meditate" <me>')
+    send_command('bind #numpad* input //send @others /ja "Sekkanoki" <me>')
+    send_command('bind #numpad- input //send @others /ja "Third Eye" <me>')
+
+    send_command('bind #numpad7 input //send @others /ws "Resolution" <t>')
+    send_command('bind #numpad4 input //send @others /ws "Catastrophe" <t>')
+    send_command('bind #numpad5 input //send @others /ws "Torcleaver" <t>')
+    send_command('bind #numpad1 input //send @others /ws "Entropy" <t>')
